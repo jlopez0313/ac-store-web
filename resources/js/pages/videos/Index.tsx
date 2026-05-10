d83353f24b8e3522b40a3df4c552d31f1be508b7
Jonathan Lopez <jlopez0313@hotmail.com>
2026-05-10 11:16:09 -0500

    Ajustes varios

--- resources/js/pages/videos/Index.tsx
+++ resources/js/pages/videos/Index.tsx
@@ -286,7 +286,6 @@ export default function VideosIndex() {
                     </div>
                 </DialogContent>
             </Dialog>
-        </div>
-    </AppLayout>
+        </AppLayout>
     );
 }
